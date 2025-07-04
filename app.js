1d2a0a91f2936280de32a35b221f4a0463434fcd
Abhay Pratap Singh <143515821+ThakurAbhay9457@users.noreply.github.com>
2025-07-04 21:26:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,10 @@ const swapRoutes = require('./routes/swap');
 // var usersRouter = require('./routes/users');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173", "http://localhost:3000"],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
