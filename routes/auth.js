11fa29366f01ba73621fc9b6415f3306aa8226cf
Abhay Pratap Singh <143515821+ThakurAbhay9457@users.noreply.github.com>
2025-07-04 21:28:24 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,8 @@ const authController = require('../controllers/authController');
 router.post('/student/signup', authController.studentSignup);
 router.post('/student/signin', authController.studentSignin);
 router.post('/admin/signin', authController.adminSignin);
+
+// WILL ADD THE GOOGLE AUTH LATER
 // router.get('/google', authController.googleAuth);
 // router.get('/google/callback', authController.googleCallback);
 router.post('/phone/signup', authController.phoneSignup);
